03b951a5ba5a7162ccef7f0a860e3c08d627d8df
TPCTPCTPC <32757790+TPCTPCTPC@users.noreply.github.com>
2024-01-22 17:59:08 +0800

    Update NFR_Overall.js

--- NFR_Overall.js
+++ NFR_Overall.js
@@ -57,7 +57,7 @@ if (!$tool.isResponse) {
             const tomatoes = IMDb.msg.tomatoes;
             const awards = IMDb.msg.awards;
             const doubanRating = Douban.rating;
-            const message = `${awards.length > 0 ? awards + "\n" + "—".repeat(awards.length).padEnd(awards.length * 2) + "\n" : ""}${IMDbrating.length > 0 ? IMDbrating : ""}${doubanRating.length > 0 ? "\t" + "|" + "\t" + doubanRating : ""}`;
+            const message = `${awards.length > 0 ? awards + "\n" : ""}${IMDbrating.length > 0 ? IMDbrating : ""}${doubanRating.length > 0 ? "\t" + "|" + "\t" + doubanRating : ""}`;
             return message;
         }
 
